614eacd0e1e089bbd72b152384bf39456cd44c74
BachMinh <bachdangthaiminh@gmail.com>
2026-04-09 10:20:12 +0200

    feat: draw random card from deck

--- table.rkt
+++ table.rkt
@@ -3,4 +3,26 @@
 (require 2htdp/universe)
 (require 2htdp/image)
 
+(define-struct card (r s))
+(define CARD-WIDTH 30)
+(define CARD-HEIGHT 50)
 
+(define spades2-big (bitmap "assets/spades_2.png"))
+(define spades2 (scale 0.5 spades2-big))
+
+(define WIDTH 500)
+(define HEIGHT 300)
+(define CANVAS (empty-scene WIDTH HEIGHT))
+
+(define (draw-card c)
+  (place-image spades2
+               100 200
+               CANVAS))
+
+(define (draw-table x)
+  (place-image (rectangle 40 20 "outline" "blue")
+               150 250
+               CANVAS))
+
+(big-bang 0
+  (to-draw draw-card))
